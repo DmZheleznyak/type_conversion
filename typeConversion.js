01cff044a4d3c4dff8f946f41151f77c34492d15
Dmitriy <11lione1991@gmail.com>
2018-11-05 17:11:13 +0200

    fixed pass

--- typeConversion.js
+++ typeConversion.js
@@ -1,45 +1,19 @@
-// class Pass {
-//   constructor(numb) {
-//     this.numb = numb
-//   }
-
-//   [Symbol.toPrimitive]( hint ) {
-//     switch( hint ) {
-//       case 'number':
-
-//         while ( this.numb < 1 ) {
-//           this.numb = this.numb + 99 
-//         }
-
-//         if ( 0 < this.numb < 100 ) {
-//           let returnValue = this.numb
-//           this.numb = this.numb + 100
-//           return returnValue
-//         }
-//     }
-//   }
-// }
-
-// var a = 6
-
-// var c = r => new Pass( r )
-
 function passTest() {
-  var value = 6
+  var value = 5
   return function() {
     var returnValue = value
     value = returnValue + 100
     return returnValue
   }
 }
-var passObjectAndArray = passTest() 
+
+var passObjectAndArray = passTest()
+var a = Object.prototype[Symbol.toPrimitive] = passObjectAndArray
 
 function test( x ) {
   return x < 100 && x > 100 ? 'pass' : 'fail'
 }
-// test( c( a ) )
- 
-Object.prototype.valueOf = Array.prototype.valueOf = passObjectAndArray
-Number.prototype.valueOf = pass 
 
-test( {} )
+test( a )
+// test( {r: "hello"} )
+// test( [3,4,5] )
