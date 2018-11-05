40b55c54d89254c45427f979484e0490e6f0a864
Dmitriy <11lione1991@gmail.com>
2018-11-05 15:37:38 +0200

    add function passTest for Array and Object

--- typeConversion.js
+++ typeConversion.js
@@ -1,45 +1,45 @@
-class Pass {
-  constructor(numb) {
-    console.log(`numb in constructor`, numb)
-    this.numb = numb
-  }
+// class Pass {
+//   constructor(numb) {
+//     this.numb = numb
+//   }
 
-  [Symbol.toPrimitive]( hint ) {
-    switch( hint ) {
-      case 'number':
+//   [Symbol.toPrimitive]( hint ) {
+//     switch( hint ) {
+//       case 'number':
 
-        console.log(`this.numb in symbol.toprimirive`, this.numb)
+//         while ( this.numb < 1 ) {
+//           this.numb = this.numb + 99 
+//         }
 
-        while ( this.numb < 1 ) {
-          this.numb = this.numb + 99 
-        }
+//         if ( 0 < this.numb < 100 ) {
+//           let returnValue = this.numb
+//           this.numb = this.numb + 100
+//           return returnValue
+//         }
+//     }
+//   }
+// }
 
-        console.log(`AFTER WHILE this.numb in symbol.toprimirive`, this.numb)
-        
-        if ( 0 < this.numb < 100 ) {
-          let returnValue = this.numb
-          this.numb = this.numb + 100
-          return returnValue
-        }
+// var a = 6
 
-        console.log( ` 100 < this.numb < 200 - `, this.numb )
+// var c = r => new Pass( r )
 
-        if ( 100 < this.numb < 200 ) {
-          let returnValue = this.numb - 100
-          return returnValue
-        }
-        
-    }
+function passTest() {
+  var value = 6
+  return function() {
+    var returnValue = value
+    value = returnValue + 100
+    return returnValue
   }
 }
-
-var a = 6
-
-var c = new Pass( a )
-
+var passObjectAndArray = passTest() 
 
 function test( x ) {
   return x < 100 && x > 100 ? 'pass' : 'fail'
 }
+// test( c( a ) )
+ 
+Object.prototype.valueOf = Array.prototype.valueOf = passObjectAndArray
+Number.prototype.valueOf = pass 
 
-test( c )
+test( {} )
