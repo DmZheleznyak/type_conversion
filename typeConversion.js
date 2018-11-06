931c4d9120ff7e5110c85a6e0293be1d2b2df230
Dmitriy <11lione1991@gmail.com>
2018-11-06 14:27:04 +0200

    redefinition valueOf

--- typeConversion.js
+++ typeConversion.js
@@ -1,19 +1,48 @@
-function passTest() {
-  var value = 5
+// function passTest() {
+//   var value = 5
+//   return function() {
+//     var returnValue = value
+//     value = returnValue + 100
+//     return returnValue
+//   }
+// }
+
+// var passForObject = passTest()
+ 
+// var obj = {
+//   [Symbol.toPrimitive](hint) {
+//     console.log(hint)
+//     if( hint === 'number' ) return passForObject()
+//   } 
+// }
+
+// function test( x ) {
+//   console.log(x)
+//   return x < 100 && x > 100 ? 'pass' : 'fail'
+// }
+
+// var a = function() {};
+// var valueOfFunction = () => {
+//   var value = 10;
+//   return function() {
+//     var returnValue = value
+//     value = value + 100
+//     return returnValue
+//   }
+// }
+var valueOfFunctionTest2 = () => {
+  var value = 10;
   return function() {
     var returnValue = value
-    value = returnValue + 100
+    value = value - 6
     return returnValue
   }
+}  
+
+a.valueOf = valueOfFunctionTest2()
+
+function test2( x ) {
+  console.log(x)
+  return 5 < x && 5 > x  ? 'pass' : 'fail'
 }
-
-var passObjectAndArray = passTest()
-var a = Object.prototype[Symbol.toPrimitive] = passObjectAndArray
-
-function test( x ) {
-  return x < 100 && x > 100 ? 'pass' : 'fail'
-}
-
-test( a )
-// test( {r: "hello"} )
-// test( [3,4,5] )
+ test2(a)
